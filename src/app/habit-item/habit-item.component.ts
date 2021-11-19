44a9ba2ef8e06ae8e0eed07be4c362a8327cc4d9
Rasoul Abdulzahra <rasoul.abdulzahra@aubay.it>
2021-11-19 12:02:07 +0100

    added habits componets

---
+++ src/app/habit-item/habit-item.component.ts
@@ -0,0 +1,20 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-habit-item',
+  template: `
+    <p>
+      habit-item works!
+    </p>
+  `,
+  styles: [
+  ]
+})
+export class HabitItemComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
